f7aa871ecbefb8969fe2de2f61195b464a17cafd
Elliot Cheung <elliotccheung@gmail.com>
2018-04-03 20:10:28 -0500

    Detects dotas correctly

--- bot.js
+++ bot.js
@@ -1,14 +1,39 @@
 const Discord = require("discord.js");
+const auth = require("./auth");
+const firebase = require("firebase");
 const client = new Discord.Client();
+
+
+firebase.initializeApp({
+ appName: "dota-button-bot",
+ databaseURL: "https:\/\/dota-button-bot.firebaseio.com/",
+});
+
+const db = firebase.database();
 
 client.on("ready", () => {
   console.log("I am ready!");
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
+  console.log("Message received");
+  console.log("User: ", message.author.id);
+  if(message.author.id==430871108716199948) return;
+
+  if (message.content.toLowerCase().includes("dota")) {
+  	let user=message.author.username;
+  	let str = `BZZZZZZZZ! ${user} mentioned DOTA!`;
+  	db.ref(`offenses/${message.guild.id}/${message.channel.name}/${user}`).once('value').then((response) => {
+  		response = response.val();
+  		let numOffenses = response.numOffenses;
+  		if(!response.numOffenses) numOffenses = 0;
+  		numOffenses += 1;
+  		str += "\n" + `${message.author.username} has committed this offense in this channel ${numOffenses} time`;
+  		if(numOffenses !== 1) str += "s";
+    	message.channel.send(str);
+  		db.ref(`offenses/${message.guild.id}/${message.channel.name}/${user}`).set({numOffenses: numOffenses});
+  	})
   }
 });
 
-client.login("NDMwODcxMTA4NzE2MTk5OTQ4.DaWfkQ.idnmtfhABtG83spxmPZFqMK8ZG4");
+client.login(auth.token);
