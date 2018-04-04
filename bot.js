b1a8824e5eca3e4e5e9887191cbdca111f335b63
Elliot Cheung <elliotccheung@gmail.com>
2018-04-03 19:23:23 -0500

    Working ping bot

--- bot.js
+++ bot.js
@@ -1,1 +1,14 @@
-bot.js
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+});
+
+client.login("NDMwODcxMTA4NzE2MTk5OTQ4.DaWfkQ.idnmtfhABtG83spxmPZFqMK8ZG4");
