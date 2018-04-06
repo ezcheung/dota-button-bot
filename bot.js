c1d1f1138952f6df2a0017e77c64820bca2bdedb
Elliot Cheung <elliotccheung@gmail.com>
2018-04-06 01:37:03 -0500

    Added responses array, and debug code

--- bot.js
+++ bot.js
@@ -10,6 +10,31 @@ firebase.initializeApp({
 
 const db = firebase.database();
 
+const responses = [
+	["Whoops! You must be new around here, ${USER}. We try not to mention DOTA so much in this channel. Have a good one :)"],
+	[
+		"Just a friendly reminder not to mention DOTA so much :smile: That's only ${TIMES} times now, so you're not in trouble yet, mkay? :smile:",
+		"Hey, ${USER}, remember that thing about mentioning DOTA? Dw, it's cool, just be a little more mindful since that's ${TIMES} times now :smile:",
+		"Oops, you mentioned DOTA again! That's ${TIMES} times, try not to do it any more :smile:"
+	],
+	[
+		"Cool it with the DOTA references, alright ${USER}? That's ${TIMES} times now", 
+		"Haha, you're just messing with me right ${USER}? That's {TIMES} times now",
+		"Heeeeeeyyyyy, ${USER}, I thought I asked you kindly not to mention DOTA. Please respect my authority around here. I don't like to be the bad guy"
+		"Dude, cmon. ${TIMES} times? Not cool."
+	],
+	[
+		"What's the matter with you, ${USER}? Are you just trying to antagonize me? That's ${TIMES} damn times now",
+		"COOL IT, ALRIGHT ${USER}? ${TIMES} times is PLENTY. Don't make me get ugly",
+		"WILL YOU STOP THAT, ${USER}? ${TIMES} times is getting a little excessive, and I'm starting to get a little mad"
+	],
+	[
+		"Alright you listen here fucko, that's ${TIMES} times. ${TIMES} GODDAMN TIMES.",
+		"@Clome BAN THIS GUY. ${TIMES} mentions of DOTA!",
+		"Oooooohhhh you cheeky bugger. ${TIMES} times? Say DOTA again. I dare you. I double dog dare you."
+	]
+]
+
 client.on("ready", () => {
 });
 
@@ -22,7 +47,7 @@ client.on("message", (message) => {
         .then(connection => {
           const dispatcher = connection.playFile('./buzzer.mp3');
         })
-        .catch((err) => {message.channel.send("Whoops, couldn't join the voice channel for some reason")});
+        .catch((err) => {message.channel.send("Whoops, couldn't join the voice channel for some reason: ", err)});
     } else {
       message.reply("No hitting the button unless you're in a voice channel");
     }
@@ -48,5 +73,9 @@ client.on("message", (message) => {
   	})
   }
 });
+
+function reply(offenses, user) {
+
+}
 
 client.login(process.env.BOT_TOKEN);
