a6bc9eada4d8f70d64997f251f98c4eee607a4db
Elliot Cheung <elliotccheung@gmail.com>
2018-04-09 00:03:57 -0500

    Fixed syntax error

--- bot.js
+++ bot.js
@@ -34,7 +34,7 @@ client.on("message", (message) => {
   }
 
   else if (message.content.startsWith("!!report")) {
-  	message.channel.send(message.author.id):
+  	message.channel.send(message.author.id)
 	if(message.author.id==210224301671055360 || message.author.id==210209958808125441) {
 		let msg = message.content;
 		let username = msg.substring(msg.indexOf(' '));
